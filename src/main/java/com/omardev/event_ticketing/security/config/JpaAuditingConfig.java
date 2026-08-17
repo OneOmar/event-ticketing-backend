9de25a42649bc6e0e7da7013420b68465c62a8b5
Omar <elmanssouriomar@gmail.com>
2026-08-17 17:53:05 +0100

    feat: add JPA auditing configuration

---
+++ src/main/java/com/omardev/event_ticketing/security/config/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package com.omardev.event_ticketing.security.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {
+}
