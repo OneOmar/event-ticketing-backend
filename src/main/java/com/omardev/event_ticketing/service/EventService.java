4e0955d350f9bbab34183d76cbbfd7cf6a967356
Omar <elmanssouriomar@gmail.com>
2026-08-21 19:22:06 +0100

    feat: add getAllEvents method with pagination to EventService

--- src/main/java/com/omardev/event_ticketing/service/EventService.java
+++ src/main/java/com/omardev/event_ticketing/service/EventService.java
@@ -3,6 +3,8 @@ package com.omardev.event_ticketing.service;
 import com.omardev.event_ticketing.dto.request.CreateEventRequest;
 import com.omardev.event_ticketing.dto.response.EventResponse;
 import com.omardev.event_ticketing.entity.Event;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 import java.util.UUID;
@@ -13,4 +15,9 @@ public interface EventService {
      * Create a new event.
      */
     EventResponse createEvent(CreateEventRequest request);
+
+    /**
+     * Retrieve all events with pagination.
+     */
+    Page<EventResponse> getAllEvents(Pageable pageable);
 }
