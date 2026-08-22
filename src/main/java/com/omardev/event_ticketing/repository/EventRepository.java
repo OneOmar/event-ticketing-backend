5d9db9036cbe8dd3483a32ebb2efc455d4e9a175
Omar <elmanssouriomar@gmail.com>
2026-08-22 16:44:46 +0100

    feat: add findByOrganizer_Id query method for events

--- src/main/java/com/omardev/event_ticketing/repository/EventRepository.java
+++ src/main/java/com/omardev/event_ticketing/repository/EventRepository.java
@@ -3,7 +3,14 @@ package com.omardev.event_ticketing.repository;
 import com.omardev.event_ticketing.entity.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface EventRepository extends JpaRepository<Event, UUID> {
+
+    /**
+     * Find events by organizer ID.
+     */
+    List<Event> findByOrganizer_Id(UUID organizerId);
+
 }
