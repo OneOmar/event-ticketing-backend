eb6ad37f15a334ae9f9ad041286e5e08774d8c5b
Omar <elmanssouriomar@gmail.com>
2026-08-29 02:26:38 +0100

    refactor(tickets): support quantity-based booking and return list of ticket responses

--- src/main/java/com/omardev/event_ticketing/service/TicketService.java
+++ src/main/java/com/omardev/event_ticketing/service/TicketService.java
@@ -3,11 +3,15 @@ package com.omardev.event_ticketing.service;
 import com.omardev.event_ticketing.dto.request.PurchaseTicketRequest;
 import com.omardev.event_ticketing.dto.response.TicketResponse;
 
+import java.util.List;
+
 public interface TicketService {
 
     /**
-     * Purchase a ticket for an event.
+     * Purchase multiple tickets for an event.
      */
 
-    TicketResponse purchaseTicket(PurchaseTicketRequest request);
+    // TicketResponse purchaseTicket(PurchaseTicketRequest request);
+
+    List<TicketResponse> purchaseTicket(PurchaseTicketRequest request);
 }
