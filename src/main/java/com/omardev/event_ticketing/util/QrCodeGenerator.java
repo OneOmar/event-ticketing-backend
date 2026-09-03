2a120962e18aa80af2f1191de2d02d2d8c616c7b
Omar <elmanssouriomar@gmail.com>
2026-09-03 22:23:07 +0100

    refactor(qr): replace RuntimeException with ApiException in QR code generator

--- src/main/java/com/omardev/event_ticketing/util/QrCodeGenerator.java
+++ src/main/java/com/omardev/event_ticketing/util/QrCodeGenerator.java
@@ -4,6 +4,7 @@ import com.google.zxing.BarcodeFormat;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
+import com.omardev.event_ticketing.exception.ApiException;
 import org.springframework.stereotype.Component;
 
 import java.io.ByteArrayOutputStream;
@@ -42,7 +43,7 @@ public class QrCodeGenerator {
 
         } catch (Exception e) {
             // Wrap and rethrow (avoid exposing low-level exception)
-            throw new RuntimeException("Failed to generate QR code", e);
+            throw new ApiException("Failed to generate QR code");
         }
     }
 }
